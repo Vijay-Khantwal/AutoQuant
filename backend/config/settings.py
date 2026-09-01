d395813e4780ef792b68a0b9ccbc98b73d5c9df1
Vijay Khantwal <vkhantwal999@gmail.com>
2026-09-01 10:56:00 +0530

    Fix requirements and database config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -60,11 +60,12 @@ TEMPLATES = [
 ASGI_APPLICATION = "config.asgi.application"
 
 # Database
+import dj_database_url
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
